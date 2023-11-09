6466a30c9296087e7647486f97c395f3ab9e6709
CasDam <c.damen@student.avans.nl>
2023-11-09 16:26:04 +0100

    Eerste opzet navigation
    UWB's kunnen communiceren met ROS, maar opzet is nog niet afgerond

--- build/navigation_pkg/catkin_generated/setup_cached.sh
+++ build/navigation_pkg/catkin_generated/setup_cached.sh
@@ -8,6 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/cas/agrobot_ws/devel/.private/navigation_pkg:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/cas/agrobot_ws/devel/.private/navigation_pkg/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/cas/agrobot_ws/devel/.private/navigation_pkg/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/cas/agrobot_ws/build/navigation_pkg'
 export ROSLISP_PACKAGE_DIRECTORIES="/home/cas/agrobot_ws/devel/.private/navigation_pkg/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/cas/agrobot_ws/src/navigation_pkg:$ROS_PACKAGE_PATH"
