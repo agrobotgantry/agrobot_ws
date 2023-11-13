aaccb2e88d267e80514c933af57389374722ed0b
CasDam <c.damen@student.avans.nl>
2023-11-13 13:57:12 +0100

    Rebuild workspace na merge

--- build/drive_pkg/catkin_generated/setup_cached.sh
+++ build/drive_pkg/catkin_generated/setup_cached.sh
@@ -8,6 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/cas/agrobot_ws/devel/.private/drive_pkg:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/cas/agrobot_ws/devel/.private/drive_pkg/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/cas/agrobot_ws/devel/.private/drive_pkg/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/cas/agrobot_ws/build/drive_pkg'
 export ROSLISP_PACKAGE_DIRECTORIES="/home/cas/agrobot_ws/devel/.private/drive_pkg/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/cas/agrobot_ws/src/drive_pkg:$ROS_PACKAGE_PATH"
